6c068f61c288836c85f4045a40c2fd59bdf7e406
Ramy Nassif <ramy@nassif.dev>
2022-10-26 17:37:39 +0000

    Completed LevelC

--- LevelC/RotationString.php
+++ LevelC/RotationString.php
@@ -15,6 +15,21 @@ class RotationString
     public static function isRotation($s1, $s2)
     {
         /** @TODO */
+        $len = strlen($s1);
+        if ($len != strlen($s2))
+            return false;
+
+        $a1 = str_split($s1);
+        $a2 = str_split($s2);
+
+        for ($i = 0; $i < $len; $i++) {
+            if ($a1 == $a2)
+                return true;
+
+            $tmp = $a1[0];
+            array_shift($a1);
+            array_push($a1, $tmp);
+        }
 
         return false;
     }
